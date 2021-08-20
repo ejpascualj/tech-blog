cbb3d447d0b0de328fd080bb3e75e8e005eaaf31
Emilio P <ejpascualj@gmail.com>
2021-08-20 16:03:54 -0500

    modified controllers

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
-const exampleRoutes = require('./exampleRoutes');
 
-router.use('/examples', exampleRoutes);
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+
+router.use('/user', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
